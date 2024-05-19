0e60c2004551ec74646c2da4e20186718f3e84d3
Auroraxpm <73517150+Auroraxpm@users.noreply.github.com>
2024-05-19 21:34:43 +0800

    chore: refine

--- config/immortalwrt-master/diy-part2.sh
+++ config/immortalwrt-master/diy-part2.sh
@@ -28,7 +28,7 @@ rm -rf package/luci-app-amlogic
 git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
 git clone https://github.com/Auroraxpm/argon package/argon
 git clone https://github.com/immortalwrt/homeproxy  package/homeproxy
-
+git clone https://github.com/vernesong/OpenClash  package/OpenClash
 #
 # Apply patch
 # git apply ../config/patches/{0001*,0002*}.patch --directory=feeds/luci
